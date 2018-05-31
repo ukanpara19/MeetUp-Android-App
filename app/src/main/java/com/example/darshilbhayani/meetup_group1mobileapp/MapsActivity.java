70685c9f16c8330f0e556e469ab1ea3e874d289f
Darshil <darshilbhayani92@gmail.com>
2018-05-31 00:53:41 -0700

    Geocoder!

--- app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/MapsActivity.java
+++ app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/MapsActivity.java
@@ -3,8 +3,11 @@ package com.example.darshilbhayani.meetup_group1mobileapp;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,7 +16,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,13 +51,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-         Drawable circleDrawable = getResources().getDrawable(R.drawable.common_google_signin_btn_icon_dark_focused);
-         BitmapDescriptor markerIcon = getMarkerIconFromDrawable(circleDrawable);
+         //Drawable circleDrawable = getResources().getDrawable(R.drawable.common_google_signin_btn_icon_dark_focused);
+         //BitmapDescriptor markerIcon = getMarkerIconFromDrawable(circleDrawable);
 
         // Add a marker in Sydney and move the camera
         LatLng latLang = new LatLng(37.349957, -121.938827);
-        mMap.addMarker(new MarkerOptions().position(latLang).title("Santa Clara Univeristy").icon(markerIcon));
+        mMap.addMarker(new MarkerOptions().position(latLang).title("Santa Clara Univeristy"));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLang,18),5000, null);
+
+        List<Address> addresses;
+        Geocoder geocoder = new Geocoder(this);
+        try {
+            addresses = geocoder.getFromLocationName("Vilnius",5);
+
+            Log.i("addresses..","."+addresses.size());
+
+            if(addresses.size() > 0)
+            {
+                Double lat = (double) (addresses.get(0).getLatitude());
+                Double lon = (double) (addresses.get(0).getLongitude());
+
+                Log.d("lat-long", "" + lat + "......." + lon);
+                final LatLng user = new LatLng(lat, lon);
+
+                /*Marker hamburg = mMap.addMarker(new MarkerOptions()
+                        .position(user)
+                        .title("2147 Newhall");
+                        .icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.common_google_signin_btn_icon_dark_focused)));*/
+                Marker hamburg = mMap.addMarker(new MarkerOptions()
+                        .position(user)
+                        .title("Vilnius"));
+                // Move the camera instantly to hamburg with a zoom of 15.
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user, 15));
+
+                // Zoom in, animating the camera.
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     private BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable) {
