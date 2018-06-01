f1724c9f671f872a481c5a582055ebdc4c00c59b
Darshil <darshilbhayani92@gmail.com>
2018-05-31 22:38:18 -0700

    ``

--- app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/MapsActivity.java
+++ app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/MapsActivity.java
@@ -95,6 +95,137 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    /*
+     // Getting Google Play availability status
+     int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+
+     // Showing status
+     if(status!=ConnectionResult.SUCCESS){ // Google Play Services are not available
+
+         int requestCode = 10;
+         Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+         dialog.show();
+
+     }else { // Google Play Services are available
+
+         Log.i("Demo!!","....................");
+
+         // Getting reference to the SupportMapFragment of activity_main.xml
+         SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+         // Enabling MyLocation Layer of Google Map
+         //mMap.setMyLocationEnabled(true);
+
+         // Opening the sharedPreferences object
+         sharedPreferences = getSharedPreferences("location", 0);
+
+         // Getting number of locations already stored
+         locationCount = sharedPreferences.getInt("locationCount", 0);
+
+         // Getting stored zoom level if exists else return 0
+         String zoom = sharedPreferences.getString("zoom", "0");
+
+         // If locations are already saved
+
+             String lat = "";
+             String lng = "";
+
+             // Iterating through all the locations stored
+             for(int i=0;i<3;i++){
+
+                 // Getting the latitude of the i-th location
+                 lat = sharedPreferences.getString("lat"+i,"0");
+
+                 // Getting the longitude of the i-th location
+                 lng = sharedPreferences.getString("lng"+i,"0");
+
+                 // Drawing marker on the map
+                 drawMarker(i,new LatLng(Double.parseDouble(lat), Double.parseDouble(lng)));
+
+                 Log.i("Demo!!","............11........");
+             }
+
+             // Moving CameraPosition to last clicked position
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(Double.parseDouble(lat), Double.parseDouble(lng))));
+             // Setting the zoom level in the map on last position  is clicked
+         mMap.animateCamera(CameraUpdateFactory.zoomTo(Float.parseFloat(zoom)));
+         }
+
+     mMap.setOnMapClickListener(new OnMapClickListener() {
+
+         @Override
+         public void onMapClick(LatLng point) {
+             locationCount++;
+
+             // Drawing marker on the map
+             drawMarker(0, point);
+
+             // Opening the editor object to write data to sharedPreferences
+             SharedPreferences.Editor editor = sharedPreferences.edit();
+
+             // Storing the latitude for the i-th location
+             editor.putString("lat"+ Integer.toString((locationCount-1)), Double.toString(point.latitude));
+
+             // Storing the longitude for the i-th location
+             editor.putString("lng"+ Integer.toString((locationCount-1)), Double.toString(point.longitude));
+
+             // Storing the count of locations or marker count
+             editor.putInt("locationCount", locationCount);
+
+             // Storing the zoom level to the shared preferences
+             editor.putString("zoom", Float.toString(mMap.getCameraPosition().zoom));
+
+             // Saving the values stored in the shared preferences
+             editor.commit();
+
+             Toast.makeText(getBaseContext(), "Marker is added to the Map", Toast.LENGTH_SHORT).show();
+
+         }
+     });
+
+     mMap.setOnMapLongClickListener(new OnMapLongClickListener() {
+         @Override
+         public void onMapLongClick(LatLng point) {
+
+             // Removing the marker and circle from the Google Map
+             mMap.clear();
+
+             // Opening the editor object to delete data from sharedPreferences
+             SharedPreferences.Editor editor = sharedPreferences.edit();
+
+             // Clearing the editor
+             editor.clear();
+
+             // Committing the changes
+             editor.commit();
+
+             // Setting locationCount to zero
+             locationCount=0;
+
+         }
+     });
+     */
+
+/* private void drawMarker(int i, LatLng point){
+     // Creating an instance of MarkerOptions
+     MarkerOptions markerOptions = new MarkerOptions();
+
+     // Setting latitude and longitude for the marker
+     markerOptions.position(point);
+     markerOptions.title("Testing Data "+i);
+     if(i==0)
+         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET));
+     else if(i==1)
+         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+     else
+         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+     // Adding marker on the Google Map
+     mMap.addMarker(markerOptions);
+ } */
+
+
+
     private BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable) {
         Canvas canvas = new Canvas();
         Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
