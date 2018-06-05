29ea9f514a2c701cb48ef5419610a4213dab31cd
dhaval <dhavalp0101@gmail.com>
2018-06-05 11:47:59 -0700

    Event POJO

---
+++ app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/Event.java
@@ -0,0 +1,17 @@
+package com.example.darshilbhayani.meetup_group1mobileapp;
+
+public class Event {
+    public String event_type;
+
+    public Event(){
+
+    }
+
+    public void setEvent_type (String event_type){
+        this.event_type = event_type;
+    }
+
+    public String getEvent_type() {
+        return event_type;
+    }
+}
