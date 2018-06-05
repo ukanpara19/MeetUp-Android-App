898e3c5a5766f58103dfd3ad926bc28995e3a084
utsav <ukanpara@scu.edu>
2018-06-04 21:27:51 -0700

    -Created UI of Main Screen

---
+++ app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/demo.java
@@ -0,0 +1,15 @@
+package com.example.darshilbhayani.meetup_group1mobileapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class demo extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+    }
+}
