0476a7ef86b7cf9f92464a32e5c8fff117f68449
parthladani <parth.ladani@gmail.com>
2018-06-08 00:12:47 -0700

    Icon Changes!

--- app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/demo.java
+++ app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/demo.java
@@ -1,15 +1,22 @@
 package com.example.darshilbhayani.meetup_group1mobileapp;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class demo extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawer);
+        setContentView(R.layout.edit_plan);
+
 
 
     }
+
 }
