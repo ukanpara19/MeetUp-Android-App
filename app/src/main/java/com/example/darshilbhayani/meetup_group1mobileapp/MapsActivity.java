61640a7ec4bf881ffb9bfe98e235f5053dfba459
dhaval <dhavalp0101@gmail.com>
2018-06-08 00:57:10 -0700

    Shared preferences editor.apply()

--- app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/MapsActivity.java
+++ app/src/main/java/com/example/darshilbhayani/meetup_group1mobileapp/MapsActivity.java
@@ -433,6 +433,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         SharedPreferences.Editor editor = getSharedPreferences(LoginDemo.MY_PREFS_NAME, MODE_PRIVATE).edit();
                         editor.putString("logged_in", "No");
                         editor.putString("Email_ID","");
+                        editor.apply();
                         startActivity(i);
                     }
 
